2885b523402862daa8b5c760557d189a2bda61de
Yoo Jinyeong <dbwlsdud456@naver.com>
2026-04-13 09:37:22 +0900

    first commit

---
+++ 01_python/script.py
@@ -0,0 +1,30 @@
+1. 프로젝트 디렉토리 생성
+~\my_proj
+
+2. 로컬 저장소(repository)생성
+  - git init
+3. 파일 생성
+   - a.txt
+4. git repository 상태 확인
+   - git status
+   - 빨강색 파일/디렉토리 -> stage에 올릴 대상.
+5. stage에 올리기
+   - git add 올릴 파일/디렉토리 목록
+    - `git add . ` 현재 디렉토리의 모든 파일/디렉토리를 다 올린다.
+
+6. git status
+   - 초록색 -> commit 대상
+
+7. commit
+   - git commit -m "커밋 메세지"
+   - git commit -m "first commit"
+
+- 파일 생성/내용 변경
+  - git status
+  - git add .
+  - git commit
+
+
+
+
+
